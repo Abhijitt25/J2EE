6899ac7a5ff8d3654bd9d3b1afab9fef576bcb18
Abhijitt25 <abhijitdevkar9696@gmail.com>
2024-01-10 15:07:03 +0530

    uploaded

--- Product_CURD/src/com/demo/services/ProductServices.java
+++
@@ -1,9 +0,0 @@
-package com.demo.services;
-
-import java.util.List;
-import com.demo.beans.*;
-public interface ProductServices {
-	List<Product> getAll();
-	Product showById(int id);
-	void addProduct(Product p);
-}
